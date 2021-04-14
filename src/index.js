fcffe8f2f3bbfd08e589b23247f194a993441322
gLightDarkness <g_light@icloud.com>
2021-04-14 19:37:20 +0900

    1行セミコロンを省いていたので追記

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import DamageCalcForm from './components/damageCalcForm'
+import DamageCalcForm from './components/damageCalcForm';
 
 ReactDOM.render(
 	<DamageCalcForm />,
