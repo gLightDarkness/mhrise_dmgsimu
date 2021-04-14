308bb75fd7c9e4bd4f68eb29631d0cbfc3d75059
gLightDarkness <g_light@icloud.com>
2021-04-14 23:13:42 +0900

    画面のレイアウトを概ね作成

---
+++ src/components/inQuestSetting.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+//import PropTypes from 'prop-types';
+
+class inQuestSetting extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                ○クエスト中効果
+            </div>
+        );
+    }
+}
+
+export default inQuestSetting;
---
+++ src/components/resultArea.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+//import PropTypes from 'prop-types';
+
+class preQuestSetting extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                ○結果
+            </div>
+        );
+    }
+}
+
+export default preQuestSetting;
