2741782cd51e8440dfe4a4d89c0ba67239aad73f
gLightDarkness <g_light@icloud.com>
2021-04-20 21:22:12 +0900

    プロトタイプデータ作成

--- src/components/weaponTypeDropdown.js
+++ src/components/weaponTypeDropdown.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import WeaponType from "../data/weapon_type.json"
 
 const WeaponTypeDropDown = (props) => {
-    const items = [
-        { id: 0, label: "武器種選択" },
-        { id: 1, label: "大剣" },
-        { id: 2, label: "太刀" },
-        { id: 3, label: "片手剣" },
-        { id: 4, label: "双剣" },
-    ];
+    const items = WeaponType;
 
     return (
         <div>
             <label>武器種: </label>
             <select onChange={(ev) => {props.handleChange(ev.target.value)}}>
                 {items.map((item) =>
-                    <option key={item.id} value={item.id}>{item.label}</option>
+                    <option key={item.id} value={item.id}>{item.name}</option>
                 )}
             </select>
         </div>
